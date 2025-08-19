6d4e6cba47b3242da8d1d06e4781b4903488d21a
João Henrique <jhjorgexs@gmail.com>
2025-08-19 15:35:44 -0300

    fix(skeleton): Correção no SkeletonMovie, ajuste na parte visual.

--- frontend/src/components/ui/SkeletonMovie.vue
+++ frontend/src/components/ui/SkeletonMovie.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <div class="w-full h-[60vh] md:h-[70vh] bg-gray-800 animate-pulse relative">
-      <div
-        class="absolute inset-0 bg-[var(--color-third)]/60 flex flex-col justify-end p-6 container mx-auto"
-      >
+      <div class="absolute inset-0 bg-[var(--color-third)]/60"></div>
+      <div class="flex flex-col justify-end p-6 container mx-auto h-full">
         <div class="h-8 md:h-12 bg-gray-600 rounded w-3/5 mb-2"></div>
         <div class="h-4 bg-gray-600 rounded w-2/5 mb-2"></div>
         <div class="flex flex-wrap gap-2 mt-2">
@@ -14,7 +13,7 @@
       </div>
     </div>
 
-    <div class="max-w-4xl mx-auto p-6 md:p-12 space-y-6">
+    <div class="container mx-auto p-6 md:p-12 space-y-6">
       <div class="h-4 bg-gray-600 rounded w-full"></div>
       <div class="h-4 bg-gray-600 rounded w-5/6"></div>
       <div class="h-4 bg-gray-600 rounded w-3/4"></div>
